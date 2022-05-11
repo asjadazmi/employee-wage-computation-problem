41c90e24bc1d01b2b120ff349f74aee3139d995b
asjadazmi <Asjadazmi0786@gmail.com>
2022-05-11 11:46:16 +0530

    employee present or not

--- Empwagecomp.sh
+++ Empwagecomp.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo "welcome to employee wage computation program on master branch ";
+
+
+
+ispresent=1;
+random=$((RANDOM%2))
+if [ $random -eq $ispresent ]
+then
+    echo "employee is present";
+else
+    echo "employee is abscent";
+
+fi
